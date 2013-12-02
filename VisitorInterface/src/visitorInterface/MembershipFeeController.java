c77775ad89d9f16a27b586550585708929ac488c
Tim Rogers <trog92@gmail.com>
2013-12-02 02:23:07 -0500

    Member form screen created. Needs implementation

--- VisitorInterface/src/visitorInterface/MembershipFeeController.java
+++ VisitorInterface/src/visitorInterface/MembershipFeeController.java
@@ -1,9 +1,30 @@
 package visitorInterface;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class MembershipFeeController implements Screen {
+	
+	ScreensController screenChanger;
 
 	@Override
 	public void setScreenParent(ScreensController controller) {
+		screenChanger = controller;
 	}
-
+	
+	@FXML
+	private void goToMain(ActionEvent e) {
+		screenChanger.setScreen(Main.MAIN_SCREEN);
+	}
+	
+	@FXML
+	private void submitMemberData(ActionEvent e) {
+		
+	}
+	
+	@FXML
+	private void getMembershipFee(ActionEvent e) {
+		System.out.println("boom");
+	}
+	
 }
