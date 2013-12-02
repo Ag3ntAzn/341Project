2f3fb30f548c272fb9e5b5b7026fe94521e4ab7d
Tim Rogers <trog92@gmail.com>
2013-12-01 23:20:47 -0500

    animal query screen functional. no query support yet

--- VisitorInterface/src/visitorInterface/AnimalController.java
+++ VisitorInterface/src/visitorInterface/AnimalController.java
@@ -1,10 +1,66 @@
 package visitorInterface;
 
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.text.Text;
+
 public class AnimalController implements Screen {
+	
+	ScreensController screenChanger;
+	
+	@FXML
+	private ListView<?> speciesList;
+	@FXML
+	private ListView<?> animalList;
+	@FXML
+	private Text errorMsg;
 
 	@Override
 	public void setScreenParent(ScreensController controller) {
-
+		screenChanger = controller;
 	}
-
+	
+	@FXML
+	private void populateSpecies(ActionEvent e) {
+		speciesList.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+		ObservableList<String> content = (ObservableList<String>) speciesList.getItems();
+		if (!content.isEmpty())
+			content.clear();
+			//content.remove(0, content.size());
+		switch (((MenuItem)e.getSource()).getText()) {
+		case "Hab 1":
+			content.addAll("Lion", "Wolf", "Tiger");
+			break;
+		case "Hab 2":
+			content.addAll("Snake", "Lizard", "Alligator");
+			break;
+		}
+		
+	}
+	
+	@FXML
+	private void searchForAnimals(ActionEvent e) {
+		ObservableList<String> selected = (ObservableList<String>) speciesList.getSelectionModel().getSelectedItems();
+		ObservableList<String> animals = (ObservableList<String>) animalList.getItems();
+		if (!animals.isEmpty())
+			animals.clear();
+		if (selected.contains("Lion")) {
+			animals.addAll("Lion 1\tMale\tA3", "Lion 2\tFemale\tA3");
+		}
+		if (selected.contains("Wolf"))
+			animals.addAll("Wolf1\tMale\tA2", "Wolf2\tFemale\tA2");
+		if (selected.contains("Snake"))
+			animals.add("Snake1\tMale\tB1");
+		if (selected.contains("Lizard"))
+			animals.add("Lizard1\tMale\tB2");
+	}
+	
+	@FXML
+	private void goToMainPage(ActionEvent e) {
+		screenChanger.setScreen(Main.MAIN_SCREEN);
+	}
 }
--- VisitorInterface/src/visitorInterface/Main.java
+++ VisitorInterface/src/visitorInterface/Main.java
@@ -4,8 +4,6 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
-import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
 	
@@ -27,7 +25,7 @@ public class Main extends Application {
 			
 			Group root = new Group();
 			root.getChildren().addAll(screensController);
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
